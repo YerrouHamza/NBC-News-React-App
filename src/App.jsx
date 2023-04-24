f4dbee5aedef6a8efadf86f66fb3d4326d2470d5
YerrouHamza <hamzayerrou5@yahoo.com>
2023-04-25 00:39:37 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from './contents/Navbar.jsx'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>Hello world</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/contents/Navbar.jsx
@@ -0,0 +1,15 @@
+import '../assets/navbar.scss'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
