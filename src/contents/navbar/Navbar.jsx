c0bb470519a8a5d0895a41a7fc76badf05a5ce0e
YerrouHamza <hamzayerrou5@yahoo.com>
2023-04-26 00:35:27 +0100

    navbar: created the navbar and the navbar style & added the logo

---
+++ src/contents/navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import './navbar.scss'
+import logo from '../../assets/nbc-logo.svg'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <a className="logo" href='#'>
+        <img src={logo} alt="" />
+      </a>
+      <nav className='menu'>
+        <ul className='navlist'>
+            <li>
+              <a className='navlist-item' href="#">Corona Updates</a>
+            </li>
+            <li>
+              <a className='navlist-item' href="#">Politics</a>
+            </li>
+            <li>
+              <a className='navlist-item' href="#">Business</a>
+            </li>
+            <li>
+              <a className='navlist-item' href="#">Sports</a>
+            </li>
+            <li>
+              <a className='navlist-item' href="#">World</a>
+            </li>
+            <li>
+              <a className='navlist-item' href="#">Travel</a>
+            </li>
+            <li>
+              <a className='navlist-item' href="#">Podcasts</a>
+            </li>
+        </ul>
+      </nav>
+      <div className="tools">
+        <a href="#" className='profile'>
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+            <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+            <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"></path>
+          </svg>
+        </a>
+        <a href="#" className='search'>
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+            <path d="M21 21l-6 -6"></path>
+          </svg>
+        </a>
+        <a href="#" className='more-tools'>
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M4 6l16 0"></path>
+            <path d="M4 12l16 0"></path>
+            <path d="M4 18l16 0"></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
