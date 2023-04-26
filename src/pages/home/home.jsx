16fffada207ab54120d9b3be57c43e3800afee90
YerrouHamza <hamzayerrou5@yahoo.com>
2023-04-27 00:22:16 +0100

    updated the navbar and the primary color

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,14 @@
-import 'home.scss';
+import './home.scss'; // import the style
+
+// import the components
 import Navbar from '../../components/navbar/Navbar';
 
 
 function home() {
   return <>
     <Navbar />
-    <div>home</div>
-    </>
+    <div className=''>home</div>
+  </>
 }
 
 export default home
