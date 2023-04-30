497d31f9c6f1177ce7e925a957e4262685911203
YerrouHamza <hamzayerrou5@yahoo.com>
2023-04-30 23:49:55 +0100

    craeted the news bar and added it to the home page

---
+++ src/components/news-bar/newsBar.jsx
@@ -0,0 +1,14 @@
+import './newsBar.scss'
+
+function NewsBar() {
+  return <>
+    <div className="news-bar">
+        <a href='#' className='btn'>
+            Breaking News
+        </a>
+        Kanye West says he's running for president in 2020.
+    </div>
+  </>
+}
+
+export default NewsBar
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,15 +2,15 @@ import './home.scss'; // import the style
 
 // import the components
 import Navbar from '../../components/navbar/Navbar'
-
 import MainPost from '../../Posts/post-main/mainPost'
-
+import NewsBar from '../../components/news-bar/newsBar';
 
 function home() {
   return <>
     <Navbar />
     <header className="home">
       <MainPost />
+      <NewsBar />
     </header>
   </>
 }
