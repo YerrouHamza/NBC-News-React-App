b3fd55a5cd10773abb1dd50950a6f70a79fc988c
YerrouHamza <hamzayerrou5@yahoo.com>
2023-05-07 16:00:21 +0100

    start geting the data from api on the filter componnent

--- src/Posts/news-filter/NewsFilter.jsx
+++ src/Posts/news-filter/NewsFilter.jsx
@@ -1,19 +1,42 @@
-// import './'
-import {useContext} from 'react';
-
-import { NewsContext } from '../../newsContext';
+import {useContext, useEffect, useState} from 'react';
+import axios from 'axios';
 
 function NewsFilter() {
 
-    const [data, setData] = useContext(NewsContext);
+    // const [data, setData] = useContext(NewsContext);
+
+    const [data, setData] = useState([]);
+    const [error, setError] = useState("");
+    const [loaded, setLoaded] = useState(false);
+
+
+    const ApiKey = '9bea390d002b45ef8bc2fe3577e52a39';
+    const Search = 'appel'
+
+    // get date and change it to the last 3 days ago date
+    const date = new Date()
+    date.setDate(date.getDate() - 3);
+    const oldDate = date.toLocaleDateString().toLocaleString()
+
+    useEffect(() => {
+        //get lsted news from API 
+        axios.get(`https://api.worldnewsapi.com/search-news?api-key=${ApiKey}&text=${Search}&earliest-publish-date=${oldDate}&number=4`)
+        .then(Response => {
+            console.log(Response.data.news)
+            setData(Response.data.news)
+            setLoaded(Response.data ? true : false)
+        }).catch(error => {
+            console.log(error)
+        })
+    }, []);
 
 
     return <div className='post-filter'>
         <div className="filter">
-            <ul>
-                <li>Latest Stories</li>
-                <li>Think</li>
-                <li>Health</li>
+            <ul className='filter-items'>
+                <li className='item'><a href="#" className='active'>Latest Stories</a></li>
+                <li className='item'><a href="#" className=''>Think</a></li>
+                <li className='item'><a href="#" className=''>Health</a></li>
             </ul>
             <button className="btn">
 
