e938c1ae0bd647e400bab68292da71195fd3aa7e
YerrouHamza <hamzayerrou5@yahoo.com>
2023-05-25 00:35:17 +0100

    added the title for the section and added the hover effect to the slider controls

--- src/Posts/news-slider/NewsSlider.jsx
+++ src/Posts/news-slider/NewsSlider.jsx
@@ -56,6 +56,7 @@ function NewsSlider() {
 
     return (
         <div className="slider">
+            <h2 className='section-title'>Editor’s Picks</h2>
             <div className="slider-container" style={sliderControl}>
                 {data.slice(0, 6).map(news => (
                     <a className="card animation-outline" key={news.id} href="">
@@ -63,7 +64,7 @@ function NewsSlider() {
                             <div className="card-img">
                                 <img src={news.image}/>
                                 <span className='card-img-icon'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                         <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
                                         <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6"></path>
