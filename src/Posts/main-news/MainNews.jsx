4ffee9cedc2f9287b2e1f386607c7bfd28b528b2
YerrouHamza <hamzayerrou5@yahoo.com>
2023-06-12 23:16:40 +0100

    updated the hero post

--- src/Posts/main-news/MainNews.jsx
+++ src/Posts/main-news/MainNews.jsx
@@ -1,6 +1,6 @@
 import './mainNews.scss'
 import postImage from '../../assets/mbc-main-post-test.png'
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 import {NewsContext} from '../../NewsContext';
 
 function MainNews() {
@@ -12,7 +12,7 @@ function MainNews() {
     <div className="post post-style-main" key={index}>
       <img className='post-img' src={news.urlToImage} alt="the Trending image" />
       <div className="post-content">
-        <h4 className='tag'>Top headline</h4>
+        <h4 className='tag'>Trending</h4>
         <h2 className='title'>
           {news.title}
         </h2>
@@ -24,7 +24,7 @@ function MainNews() {
             <span>{'2'}</span> hours ago
           </p>
           <p className='posted-by'>
-           By <span>{news.author}</span>
+           By <span>{news.creator ? news.creator : 'Anonymous'}</span>
           </p>
         </div>
       </div>
