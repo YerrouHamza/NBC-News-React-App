6b71514927e591214ac45ab430bc28b3f1159efe
YerrouHamza <hamzayerrou5@yahoo.com>
2023-06-25 16:16:52 +0100

    clean the navbar code and added the comments

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,13 @@
 import { useState, useContext, useEffect } from 'react';
-
 import { Link } from 'react-router-dom'
 
-import './navbar.scss'
-import logo from '../../assets/nbc-logo.svg'
-
 import { NewsContext } from '../../NewsContext'; // import the data from the context state
- // import the data from the context state
+
+import './navbar.scss'; // import the Navbar style
+import logo from '../../assets/nbc-logo.svg'; // import the logo
+import { IconMenu2, IconSearch, IconUserCircle } from '@tabler/icons-react'; // import the icons form the https://tabler-icons.io/
 
 function Navbar({activeItem}) {
-  
   
   const [activeCategory, setActiveCategory] = useState()
   const {globalSettings, setGlobalSettings} = useContext(NewsContext);
@@ -56,27 +54,13 @@ function Navbar({activeItem}) {
         </nav>
         <div className="tools">
           <a href="#" className='profile'>
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-user-circle" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
-              <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
-              <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"></path>
-            </svg>
+            <IconUserCircle />
           </a>
           <a href="#" className='search'>
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
-              <path d="M21 21l-6 -6"></path>
-            </svg>
+            <IconSearch />
           </a>
           <a href="#" className='more-tools' onClick={handelOpenGlobalSettings}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M4 6l16 0"></path>
-              <path d="M4 12l16 0"></path>
-              <path d="M4 18l16 0"></path>
-            </svg>
+            <IconMenu2 />
           </a>
         </div>
       </div>
